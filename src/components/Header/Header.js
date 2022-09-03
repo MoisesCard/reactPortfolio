78f5380e8f39003ef76479b3cca10de4ce630de9
KingMo <macv123321@gmail.com>
2022-09-03 01:36:36 -0400

    added css to header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import "./header.css"
+import CTA from './CTA'
+import ME from '../../assets/ME.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className='container header_container'>
+        <h5>Hello I'm</h5>
+        <h1>Moises Cardenas</h1>
+        <h5 className='text-light'>Fullstack Developer</h5>
+        <CTA/>
+        <HeaderSocials/>
+
+        <div className='me'>
+          <img src={ME} alt='Photo of Moises'></img>
+        </div>
+        <a href='#contact' className='scroll-down'>Scroll down</a>
+      </div>
+    </header>
   )
 }
 
