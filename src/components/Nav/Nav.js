d2827e4302fe2f1938325473455cd1e90f9a066a
KingMo <macv123321@gmail.com>
2022-09-03 16:29:47 -0400

    worked on nav component

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import "./nav.css"
+import {AiOutlineHome}  from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BsBook} from 'react-icons/bs'
+import {AiOutlineFolderOpen} from 'react-icons/ai'
+import {AiOutlineContacts} from 'react-icons/ai'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}>
+        <AiOutlineHome/></a>
+      <a href='#About' onClick={() => setActiveNav('#About')} className={activeNav === '#About' ? 'active' : ''}>
+        <AiOutlineUser/></a>
+      <a href='#Experience' onClick={() => setActiveNav('#Experience')} className={activeNav === '#Experience' ? 'active' : ''}>
+        <BsBook/></a>
+      <a href='#Portfolio'onClick={() => setActiveNav('#Portfolio')} className={activeNav === '#Portfolio' ? 'active' : ''}>
+        <AiOutlineFolderOpen/></a>
+      <a href='#Contact' onClick={() => setActiveNav('#Contact')} className={activeNav === '#Contact' ? 'active' : ''}>
+        <AiOutlineContacts/></a>
+    </nav>
   )
 }
 
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -3,7 +3,7 @@ import "./portfolio.css"
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section id='Portfolio'>Portfolio</section>
   )
 }
 
