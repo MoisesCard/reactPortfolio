b0683ce7f374388a1f3ab8e5827db260ce180119
KingMo <macv123321@gmail.com>
2022-09-03 00:30:50 -0400

    added to index.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import react from 'react'
+import Header from "./components/Header/Header"
+import Nav from "./components/Nav/Nav"
+import Contact from "./components/Contact/Contact"
+import Experience from "./components/Experience/Experience"
+import About from "./components/About/About"
+import Portfolio from "./components/Portfolio/Portfolio"
+import Footer from "./components/Footer/Footer"
 
 const App = () => {
   return (
-    <div>
-      App
-      </div>
+    <>
+      <Header/>
+      <Nav/>
+      <About/>
+      <Experience/>
+      <Portfolio/>
+      <Contact/> 
+      <Footer/>
+    </>
   )
 }
 
