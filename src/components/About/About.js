f431e521f2ae05407873e40266975bb6a1680424
KingMo <macv123321@gmail.com>
2022-09-04 11:13:05 -0400

    styled about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,53 @@
 import React from 'react'
 import "./about.css"
+import ME2 from '../../assets/ME2.png'
+import {MdOutlineComputer}  from 'react-icons/md'
+import {HiOutlineFolderOpen} from 'react-icons/hi'
 
 const About = () => {
   return (
-    <section id='About'>About</section>
+    <section id='About'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className='container about_container'>
+        <div className='about_me'>
+          <div className='about_me-image'>
+            <img src={ME2} alt='Second photo of Moises'></img>
+          </div>
+        </div>
+
+        <div className='about_content'>
+          <div className='about_cards'>
+            <article className='about_card'>
+              <MdOutlineComputer className='about_icon'/>
+              <h5>Experience</h5>
+              <small>1 Year</small>
+            </article>
+
+            <article className='about_card'>
+              <HiOutlineFolderOpen className='about_icon'/>
+              <h5>Projects</h5>
+              <small>20+ Completed Projects</small>
+            </article>
+
+            {/* <article className='about_card'>
+              <GrPersonalComputer className='about_icon'/>
+              <h5>Experience</h5>
+              <small>1 year</small>
+            </article> */}
+          </div>
+
+          <p>
+              lllkmgslfkgnldrjnglknrglkdnfgsldknfglskdng sdlfkgnsdlfgkhns 
+              dkfjgnksfdg lsfhkgn sldkfg nlsdfkgnsldfgknsdlfk 
+              gnsldfkg nlskfd ng
+          </p>
+
+          <a href='#Contact' className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
